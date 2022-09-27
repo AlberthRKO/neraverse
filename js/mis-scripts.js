cd996cf18a535c61c7bf705753998cec20fd7bfd
Alberth <orlando.alb77@gmail.com>
2022-09-27 16:05:29 -0400

    arreglos de animacion de entrada

--- js/mis-scripts.js
+++ js/mis-scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
   $("#example").DataTable();
   let modalsito = $("#staticBackdrop");
   modalsito.addClass(modalsito.attr("data-animate-in"));
-});
+}); */
 $(window).on("load", function () {
   if ($("#preloader").length) {
     $("#preloader")
